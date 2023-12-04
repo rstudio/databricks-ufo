0e2f76469711d159a65de5a9c40e19615333c86b
Daniel Chen <chendaniely@users.noreply.github.com>
2023-12-04 14:57:12 -0800

    ufo dashboard and shiny app :tada:

---
+++ deploy.R
@@ -0,0 +1,21 @@
+# make sure you run these commands from the root of the repo as the working directory
+
+rsconnect::deployApp(
+  appName = "ufo-shiny",
+  appTitle = "UFO Report Explorer",
+  appDir = here::here("ufo-shiny"),
+  # set the python path (you need to change this value yourself
+  python = "/Users/danielchen/.virtualenvs/r-sparklyr-databricks-14.1/bin/python",
+  envVars = c("DATABRICKS_HOST", "DATABRICKS_TOKEN", "DATABRICKS_CLUSTER_ID"),
+  lint = FALSE
+)
+
+rsconnect::deployApp(
+  appName = "ufo-dashboard",
+  appTitle = "Reported UFO sightings",
+  appDir = here::here("ufo-dashboard"),
+  # set the python path (you need to change this value yourself
+  python = "/Users/danielchen/.virtualenvs/r-sparklyr-databricks-14.1/bin/python",
+  envVars = c("DATABRICKS_HOST", "DATABRICKS_TOKEN", "DATABRICKS_CLUSTER_ID"),
+  lint = FALSE
+)
